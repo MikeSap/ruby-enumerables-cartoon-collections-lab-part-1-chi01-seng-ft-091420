20a1f9de74e9994be183d1470740567519fafb8a
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-03 23:25:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |char, index|
-    array[char] = 1
+    array[index] = 1
       puts char
     end
   end
