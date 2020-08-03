0c1dadb55b374f3966a027249fd4b05b3462fe12
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-03 20:55:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |char|
-    puts "Hello, I am " + char
+    p "Hello, I am " + char
   end
 end
 
@@ -11,4 +11,6 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each do |char|
+    puts char
 end
